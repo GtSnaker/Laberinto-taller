f07b1ff8d76599bb1d392b2eb21d45f74813399f
Gtsnaker <gtsnaker@gmail.com>
2014-04-29 15:29:48 +0200

    función textizador

--- src/js/dialogo1.js
+++ src/js/dialogo1.js
@@ -11,12 +11,23 @@
     create: function () {
       var x = this.game.width / 2
         , y = this.game.height / 2;
-
-      this.charBool = true;
+      
+      this.timePerChar = 0;
+      this.timeAhorita = 0;
 
       this.box = this.add.sprite(x, y, 'textBox');
       this.box.x = 25;
       this.box.y = 435;
+
+      this.dialogoTotal = '¡Hey! ¡Hola chico!\n¿Tú no eres algo pequeño para estar por aqui solo? \n¡No me digas que eres ese niño de la leyenda de la cueva! \n¡Ese que devora caballos y rompe piedras a mordiscos!\n¿Eres tú?';
+      this.stateText = this.game.add.text(this.game.world.centerX, this.game.world.centerY,'', { fontSize: '84px', fill: '#fff' });
+      this.dialogoText = this.game.add.text(40, 450, '', { fontSize: '34px', fill: '#fff' });
+      this.dialogoText.fontSize = 27;
+      this.textoTotal = '';
+      this.ntotal = 0;
+
+      this.charBool = true;
+
       this.unicornioChar = this.add.sprite(x, y, 'unicornioPeta');
       this.unicornioChar.x = 707;
       this.unicornioChar.y = 317;
@@ -30,11 +41,18 @@
 
     update: function () {
       
+      this.stateText.anchor.setTo(0.5, 0.5);
+      this.stateText.visible = false;
+
+      this.textizador(this.dialogoTotal, 5000);
+      this.dialogoText.text = this.textoTotal;
+      
+      
+      //console.log(this.textizador(this.dialogoTotal, 5000));   
     },
 
     onDown: function () {
       this.charBool = !this.charBool;
-      console.log(this.charBool);
       if (!this.charBool) {
         this.unicornioChar.exists = false;
         this.slenderChar.exists = true;
@@ -43,7 +61,23 @@
         this.unicornioChar.exists = true;
         this.slenderChar.exists = false;
       }
-    }
+    },
+
+    // función que transforma this.textoTotal en la variable string que se le pase por primer parámetro
+    // Tiempo en milisegundos en los que se ejecuta, pasado en milisegundos en el segundo parámetro
+    textizador: function (texto, tiempo) {
+      this.ncaracteres = texto.length;
+      this.timePerChar = tiempo / this.ncaracteres;
+      this.timeAhorita = this.time.now;
+      if (this.ntotal < this.ncaracteres){
+        if ((this.time.now - this.timeAhorita) < this.timePerChar) {
+          this.textoTotal += this.dialogoTotal[this.ntotal];
+          this.ntotal++;
+          this.timeAhorita = this.time.now;
+        }
+      }
+    },
+
   };
 
   window['laberinto'] = window['laberinto'] || {};
